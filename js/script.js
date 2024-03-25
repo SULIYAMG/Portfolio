9d295610f0f6b91018f28f40ffb2c890010aefae
SULIYAMG <supriyamg132001@gmail.com>
2024-03-25 11:44:32 +0530

    using html css

---
+++ js/script.js
@@ -0,0 +1,30 @@
+(function (){
+    //theme switching
+    document.querySelector('.theme-btn').addEventListener('click',()=>{
+        document.body.classList.toggle('light-mode');
+    });
+    [...document.querySelectorAll('.control')].forEach(item => {
+        item.addEventListener('click',function(){
+            //remove active-btn class from active link
+            document.querySelector('.active-btn').classList.remove('active-btn');
+            //adding active-btn to current clicked link
+            this.classList.add('active-btn')
+            document.querySelector('.active').classList.remove('active')
+            //add to the section which match section id with data set id
+            document.getElementById(item.dataset.id).classList.add('active')
+        })
+    })
+})()
+
+function handler () {
+    [...document.querySelectorAll('.social-icon')].forEach(item => {
+        // console.log(`item =`, item.classList)
+        item.addEventListener("click",function(){
+            document.querySelector('.active').classList.remove('active');
+            console.log(`single=`, this.classList)
+            this.classList.add('active')
+        })
+    })
+}
+
+handler()
